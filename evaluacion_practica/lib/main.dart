8101365c8f483e7cfc672ca39f7ede736a015b9b
Andres Almeida <asalmeida4@espe.edu.ec>
2024-06-09 18:13:19 -0500

    Update main.dart

--- evaluacion_practica/lib/main.dart
+++ evaluacion_practica/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Kids Game',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
       home: PaginaPrincipal(),
     );
